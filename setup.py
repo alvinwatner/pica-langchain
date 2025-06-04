062f4d8ad158e7c0390d11300c34411b4a76bd50
alvinwatner <alvinsetiadi22@gmail.com>
2025-06-04 11:54:34 +0700

    chore: remove google-serper from setup.pu

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
         "langchain_openai==0.3.8",
         "langchain-community>=0.2.0",
         "duckduckgo-search>=4.1.0",
-        "google-serper>=0.1.0",
         "pydantic==2.10.6",
         "requests==2.32.3",
         "requests-toolbelt>=1.0.0",
